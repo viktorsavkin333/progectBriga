9b1850c0e1b139c82df56f6b395aa6b4e35dd6ee
VIKYT <82964583+viktorsavkin333@users.noreply.github.com>
2024-07-11 13:19:01 +0300

    Update courseee.py

--- progectbirga1/courseee.py
+++ progectbirga1/courseee.py
@@ -1,1 +1,15 @@
-import randomimport timewhile True:    aktioprise = 100    b = random.randint(0, 1)    if b == 0:        aktioprise = aktioprise - random.random()        print(f'Сейчас 1 акция стоит: {aktioprise}', end="\b")        time.sleep(2.5)        print('', end="\r")    elif b == 1:        aktioprise = aktioprise + random.random()        print(f'Сейчас 1 акция стоит: {aktioprise}', end="\b")        time.sleep(2.5)        print('', end="\r")
+import random
+import time
+while True:
+    aktioprise = 100
+    b = random.randint(0, 1)
+    if b == 0:
+        aktioprise = aktioprise - random.random()
+        print(f'Сейчас 1 акция стоит: {aktioprise}', end="\b")
+        time.sleep(2.5)
+        print('', end="\r")
+    elif b == 1:
+        aktioprise = aktioprise + random.random()
+        print(f'Сейчас 1 акция стоит: {aktioprise}', end="\b")
+        time.sleep(2.5)
+        print('', end="\r")
