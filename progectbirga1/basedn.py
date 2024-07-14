a77a1dcd29b7840c8798c932a8b43e4a0fe47d36
VIKYT <82964583+viktorsavkin333@users.noreply.github.com>
2024-07-14 21:19:57 +0300

    Update basedn.py

--- progectbirga1/basedn.py
+++ progectbirga1/basedn.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 global enter 
-help = "Command List: /help - Show full list of commands, /new_user - Create new user, /login - Login to user"
+
 
 global db
 global sql
@@ -17,45 +17,77 @@ sql.execute("""CREATE TABLE IF NOT EXISTS users (
 )""")
 
 def login():
-    user_login = input("Enter login: ")
-    user_password = input("Enter password: ")
+    global user_login, user_password
+    user_login = input("Введите логин: ")
+    user_password = input("Введите пароль: ")
     sql.execute(f"SELECT login FROM users WHERE login = '{user_login}'")
     if sql.fetchone() is None:
-        print("This user does not exist")
-        main()
+        print("Такого пользователя не существует")
+        main(False)
     else:
-        sql_update_query = f"""Update users set diamond = diamond-1  where login = {user_login}"""
+        registred = True
+        sql_update_query = f"""UPDATE users set diamond = diamond+1 WHERE login = {user_login}"""
+        allselect = f"""SELECT * FROM users WHERE login={user_login}"""
+        sql.execute(allselect)
+        records = sql.fetchall()
+        print(records)
+        print(type(records))
+        print(records[0][-1])
         sql.execute(sql_update_query)
         db.commit()
         print("Запись успешно обновлена")
-        sql.close()
+        main(True)
 
 def reg():
-    user_login = input("Enter login: ")
-    user_password = input("Enter password: ")
+    user_login = input("Введите логин: ")
+    user_password = input("Введите пароль: ")
     diamond = 100
     airon = 0
     abc(user_login, user_password, diamond, airon)
 
-def main():
-        print(help)
-        enter = input("Enter the command: ")
-        if enter == '/help':
+def main(registred):
+        if registred == False:
+            help = "Список команд: /help - Команды, /new_user - Регистрация, /login - Вход"
             print(help)
-        elif enter == '/new_user':
-            reg()
-        elif enter == '/login':
-            login()
+            enter = input("Введите команду: ")
+            if enter == '/help':
+                print(help)
+            elif enter == '/new_user':
+                reg()
+            elif enter == '/login':
+                login()
+            else:
+                print('Неверная команда')
+                main()
+        if registred == True:
+            help = "Список команд: /help - Команды, /user - Профиль, /birsh - Перейти на биржу"
+            print(help)
+            enter = input("Введите команду: ")
+            if enter == '/help':
+                print(help)
+            elif enter == '/user':
+                user()
+            else:
+                print('Неверная команда')
+                main()
 
 def abc (user_login, user_password, diamond, airon):
         sql.execute(f"SELECT login FROM users WHERE login = '{user_login}'")
         if sql.fetchone() is None:
             sql.execute(f"INSERT INTO users VALUES (?, ?, ?, ?)", (user_login, user_password, diamond, airon))
             db.commit()
-            print('You have successfully registered')
-            main()
+            print('Вы успешно зарегестрированны')
+            main(False)
         else:
-            print('Such a record already exists')
-            main()
+            print('Такая учетная запись уже существует')
+            main(False)
 
-main()
+def user():
+    allselect = f"""SELECT * FROM users WHERE login={user_login}"""
+    sql.execute(allselect)
+    records = sql.fetchall()
+    print(records)
+    print(type(records))
+    print(records[0][-1])
+    main(True)
+main(False)
