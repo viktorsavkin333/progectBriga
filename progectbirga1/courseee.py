ef19a9c4cfd18dc737f073d99df1bf36f23567a5
VIKYT <82964583+viktorsavkin333@users.noreply.github.com>
2024-07-14 14:40:13 +0300

    Update courseee.py

--- progectbirga1/courseee.py
+++ progectbirga1/courseee.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-import randomimport timewhile True:    aktioprise = 100    b = random.randint(0, 1)    if b == 0:        aktioprise = aktioprise - random.random()        print(f'Сейчас 1 акция стоит: {aktioprise}', end="\b")        time.sleep(2.5)        print('', end="\r")    elif b == 1:        aktioprise = aktioprise + random.random()        print(f'Сейчас 1 акция стоит: {aktioprise}', end="\b")        time.sleep(2.5)        print('', end="\r")
-=======
+
 import random
 import time
+
 while True:
     aktioprise = 100
     b = random.randint(0, 1)
@@ -16,4 +15,5 @@ while True:
         print(f'Сейчас 1 акция стоит: {aktioprise}', end="\b")
         time.sleep(2.5)
         print('', end="\r")
->>>>>>> 62676a77216200dcad948c2129e5e52834199dde
+
+
